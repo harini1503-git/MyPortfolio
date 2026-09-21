d193d02b052667c967e1c091cd91aaa66407f06f
harini1503-git <155567631+harini1503-git@users.noreply.github.com>
2026-09-21 22:46:07 +0530

    Update resume link in About component

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/12V4hHNEkBEWy7jACzQVg5WKz3DGRInte/view?usp=share_link"
+            href="https://drive.google.com/file/d/1DipU8pPB1LN2BRptMWHnKLrKL5S6Lecr/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
